ccb8d4b10b3ce882e0b4855288d5d020fe2a8f67
zeinhum <zeinmosey@gmail.com>
2024-09-13 15:59:48 +1200

    higher lower game v.1.0

---
+++ play.py
@@ -0,0 +1,38 @@
+from game_data import data
+import random
+
+print("** let's play higher-lower game, you choose who has more followers")
+
+
+
+score = 0
+
+
+def play_game():
+    global score
+    while True:
+        player_a, player_b = random.sample(data, 2)
+
+        print(f"Option A: {player_a['name']}, {player_a['description']} from {player_a['country']}")
+        print(f"Option B: {player_b['name']}, {player_b['description']} from {player_b['country']}")
+
+        option = input("** choose your option: ").lower().strip()
+        if option == 'a':
+            if player_a['follower_count'] > player_b['follower_count']:
+                score += player_a['follower_count']
+                print(f"nice ! your score is {score}")
+            else:
+                print(f"oops! wrong gase, you score is {score}")
+                break
+
+        if option == 'b':
+            if player_b['follower_count'] > player_a['follower_count']:
+                score += player_a['follower_count']
+                print(f"nice ! your score is {score}")
+            else:
+                print(f"oops! wrong gase, you score is {score}")
+                break
+        if option == 's':
+            break
+
+play_game()
